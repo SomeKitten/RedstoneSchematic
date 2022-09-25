b76dd2ea4f4708bdaf3475c7b9a65736c0235a4e
Some Kitten <blueredkitten@gmail.com>
2022-09-24 20:29:45 -0400

    Added wide (larger than two input) gate support

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import mcschematic
 import circuitgraph as cg
 from logic_schematics import LogicSchematics
 
-input_verilog = "test2"
+input_verilog = "alu"
 
 # change this dir to yur own schematic folder
 output_dir = "/home/kitten/.local/share/multimc/instances/1.17.1/.minecraft/config/worldedit/schematics"
@@ -13,7 +13,11 @@ output_dir = "/home/kitten/.local/share/multimc/instances/1.17.1/.minecraft/conf
 schem = mcschematic.MCSchematic()
 
 # circuit_from_verilog = cg.from_file(f"./verilog/{input_verilog}.v")
-circuit_from_verilog = cg.logic.full_adder()
+# circuit_from_verilog = cg.logic.mux(3)
+# circuit_from_verilog = cg.logic.full_adder()
+circuit_from_verilog = cg.logic.adder(4)
+
+cg.to_file(circuit_from_verilog, "verilog/generated.v")
 
 
 class Node:
@@ -43,7 +47,7 @@ def recursive_process(circuit: circuitgraph.Circuit, depth_nodes: list[list[Node
         if node not in nodes_to_names(depth_nodes[depth]):
             fan = circuit.fanout(node)
             if circuit.is_output(node):
-                depth_nodes[depth] = [Node(circuit.type(node), node, [], fan)] + depth_nodes[depth]
+                depth_nodes[depth] = [Node(circuit.type(node), node, [], set())] + depth_nodes[depth]
             else:
                 depth_nodes[depth] += [Node(circuit.type(node), node, [], fan)]
             if len(fan) > 0:
@@ -56,6 +60,55 @@ def redundant_node_deletion(depth_nodes: list[list[Node]]):
         index = len(depth_nodes) - index - 1
         depth_nodes[index] = [node for node in layer if node.name not in s]
         s |= set(nodes_to_names(layer))
+
+
+def wide_gate_splitter(circuit: circuitgraph.Circuit, depth_nodes: list[list[Node]]):
+    node_input_amount = {}
+    index = 0
+    while index < len(depth_nodes):
+        current_layer = depth_nodes[index]
+        for i, node in enumerate(current_layer):
+            typ, name, data, output = node.to_tuple()
+            if typ == "buf" or typ == "tunnel":
+                continue
+
+            print(f"Processing {typ} {name} at {index} {i}")
+            print(f"output: {output}")
+
+            for out in output.copy():
+                if out not in node_input_amount:
+                    node_input_amount[out] = 0
+
+                node_input_amount[out] += 1
+
+                if node_input_amount[out] > 2:
+                    print(f"{out} has {node_input_amount[out]} inputs")
+
+                    out_node, out_index = get_node(depth_nodes, out)
+                    next_node = Node(out_node.typ, f"{out}_ext_{node_input_amount[out] - 3}", [], out_node.output)
+                    out_node.output = {next_node.name}
+                    output.remove(out)
+                    output.add(next_node.name)
+
+                    new_layer = [next_node]
+                    for node_inner in current_layer:
+                        if len(node_inner.output) == 0:
+                            new_layer = [Node("wire", "", [], set())] + new_layer
+                        else:
+                            break
+
+                    depth_nodes.insert(out_index + 1, new_layer)
+                    depth_nodes.insert(out_index + 1, [])
+            print(f"output: {output}")
+        index += 2
+
+
+def get_node(depth_nodes: list[list[Node]], name: str):
+    for index, layer in enumerate(depth_nodes):
+        for node in layer:
+            if node.name == name:
+                return node, index
+    raise KeyError(f"Node {name} not found")
 
 
 def tunnel_generation(depth_nodes: list[list[Node]]):
@@ -83,7 +136,7 @@ def tunnel_generation(depth_nodes: list[list[Node]]):
                 tunnel_amount += 1
 
 
-def output_generation(circuit: circuitgraph.Circuit, depth_nodes: list[list[Node]]):
+def output_generation(depth_nodes: list[list[Node]]):
     for depth in range(len(depth_nodes) // 2):
         index = depth * 2 + 1
 
@@ -96,13 +149,13 @@ def output_generation(circuit: circuitgraph.Circuit, depth_nodes: list[list[Node
         for i, node in enumerate(depth_nodes[index - 1]):
             typ, name, data, output = node.to_tuple()
 
-            if typ == "wire" or name not in circuit.nodes():
+            if typ == "wire" or len(output) > 0:
                 continue
-            if circuit.is_output(name):
-                depth_nodes[index][i] = Node("output", "", [], set())
-                if after_len >= before_len:
-                    depth_nodes[index] += [Node("wire", "", [], set())]
-                shift_right(depth_nodes, index + 1)
+
+            depth_nodes[index][i] = Node("output", "", [], set())
+            if after_len >= before_len:
+                depth_nodes[index] += [Node("wire", "", [], set())]
+            shift_right(depth_nodes, index + 1)
 
 
 # generate redstone wires
@@ -184,7 +237,6 @@ def create_wire(location: tuple[int, int, int], path: list[tuple[str, int | tupl
                 "forward": "backward", "backward": "forward"
             }[direction]
             length = -length
-
 
         if direction == "up":
             # generate glass ladder
@@ -291,8 +343,9 @@ def main(circuit: circuitgraph.Circuit):
     depth_nodes: list[list[Node]] = []
     recursive_process(circuit, depth_nodes, circuit.inputs(), 0)
     redundant_node_deletion(depth_nodes)
+    wide_gate_splitter(circuit, depth_nodes)
     tunnel_generation(depth_nodes)
-    output_generation(circuit, depth_nodes)
+    output_generation(depth_nodes)
     wire_generation(circuit, depth_nodes)
 
     for depth, nodes in enumerate(depth_nodes):
